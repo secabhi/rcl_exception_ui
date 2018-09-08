9b5935e5c780c128437169ebcf00abfdb0a45577
Ravi Teja Mannava <ravistack4@gmail.com>
2018-09-09 02:39:03 +0530

    webpack added to create-react-app

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+module.exports = {
+  entry: ['./src/index.js'],
+  output: {
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015', 'stage-1']
+        }
+      },
+      {
+        test: /\.css$/,
+        loader: "style-loader!css-loader" 
+      },
+      {
+        test: /\.json$/,
+        loader: 'json-loader'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './',
+    watchOptions: {
+      aggregateTimeout: 300,
+      poll: 1000
+    }
+  }
+};
